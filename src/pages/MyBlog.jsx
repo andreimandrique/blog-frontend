889fedac21329dfbb86da253298d22f81d14f283
Andrei <andreimandrique3@gmail.com>
2025-04-11 22:11:03 +0800

    add AddBlog and MyBlog

---
+++ src/pages/MyBlog.jsx
@@ -0,0 +1,12 @@
+import LogoutButton from "../components/LogoutButton.jsx";
+
+function MyBlog() {
+  return (
+    <div>
+      <h1>My Blog</h1>
+      <LogoutButton/>
+    </div>
+  )
+}
+
+export default MyBlog;
